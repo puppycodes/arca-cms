e8bf5c3fe94db07c6011194e8ff2d4a018f28cd7
puppycodes <woof@puppy.codes>
2020-03-05 15:47:00 -0500

    init

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+var support = (function () {
+	if (!window.DOMParser) return false;
+	var parser = new DOMParser();
+	try {
+		parser.parseFromString('x', 'text/html');
+	} catch(err) {
+		return false;
+	}
+	return true;
+})();
+
+/**
+ * Convert a template string into HTML DOM nodes
+ * @param  {String} str The template string
+ * @return {Node}       The template HTML
+ */
+var stringToHTML = function (str) {
+
+
+	// Otherwise, fallback to old-school method
+	var dom = document.createElement('div');
+	dom.innerHTML = str;
+	return dom;
+
+};
